32666ea6bb11d15e81a40f0f3b203f1d7a45c453
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-01-10 12:11:18 +0300

    [TSKs/LES-2.5] data-types, 1 of 1
    Interaction with str's, ${..} template.
    
    Deserv(s) attention:
      - data type knowledge (a few subtleties with "null" and "function").
    
    part 1 / JS

---
+++ part-1-js/2-js-basics/2-5-data-types/index.js
@@ -0,0 +1,8 @@
+// Шаблонные строки
+// Что выведет этот скрипт?
+
+let myName = 'Roni';
+
+console.log(`Hello ${1}!`); // Hello 1!
+console.log(`Hello ${'myName'}..`); // Hello myName..
+console.log(`Hello ${myName}!!`); // Hello Roni!!
