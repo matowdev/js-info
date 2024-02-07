6e404fa78f4d1745a20eac2f1bce5c2539997568
s2kdev <79189126+s2kdev@users.noreply.github.com>
2024-02-07 16:34:43 +0300

    [TSK/LES-4.2/ret] Add some/other solutions
    Adding two alternative entries/solutions.
    
    part 3 / add

--- part-3-add/4-storing-data-in-browser/4-2-localStorage-sessionStorage/index.js
+++ part-3-add/4-storing-data-in-browser/4-2-localStorage-sessionStorage/index.js
@@ -4,3 +4,17 @@ area.value = localStorage.getItem('area');
 area.oninput = () => {
   localStorage.setItem('area', area.value);
 };
+
+// * альтернативные решение, вместо стрелочной функции
+
+// ? использовать анонимную
+// area.oninput = function() {
+//   localStorage.setItem('area', area.value);
+// };
+
+// ? использовать именованную функцию, с последующим присвоением (в качестве обработчика события)
+// function saveText() {
+//   localStorage.setItem('area', area.value);
+// }
+
+// area.oninput = saveText;
