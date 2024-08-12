b955fd018e721a761248e259dfc832f22278e293
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2024-08-12 07:41:51 +0300

    [TSK/LES-11.8] async-await, 2 of 3
    Conversion func to async/await logic, "then" exception.
    
    Worth noting:
      - only solution from JS Info.
    
    part 1 / JS

---
+++ part-1-js/11-promises/11-8-async-await/rewriteUsingAsyncAwait2.js
@@ -0,0 +1,89 @@
+// Перепишите, используя async/await
+// Ниже пример из раздела Цепочка промисов, перепишите его, используя async/await вместо .then/catch.
+// В функции demoGithubUser замените рекурсию на цикл: используя async/await, сделать это будет просто.
+
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+function loadJson(url) {
+  return fetch(url).then((response) => {
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      throw new HttpError(response);
+    }
+  });
+}
+
+// Запрашивать логин, пока github не вернёт существующего пользователя.
+function demoGithubUser() {
+  let name = prompt('Введите логин?', 'iliakan');
+
+  return loadJson(`https://api.github.com/users/${name}`)
+    .then((user) => {
+      alert(`Полное имя: ${user.name}.`);
+      return user;
+    })
+    .catch((err) => {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert('Такого пользователя не существует, пожалуйста, повторите ввод.');
+        return demoGithubUser();
+      } else {
+        throw err;
+      }
+    });
+}
+
+demoGithubUser();
+
+// Решение JS Info
+// В этой задаче нет ничего сложного. Нужно заменить .catch на try...catch внутри demoGithubUser и добавить async/await, где необходимо:
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+async function loadJson(url) {
+  let response = await fetch(url);
+
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+// Запрашивать логин, пока github не вернёт существующего пользователя.
+async function demoGithubUser() {
+  let user;
+
+  while (true) {
+    let name = prompt('Введите логин?', 'iliakan');
+
+    try {
+      user = await loadJson(`https://api.github.com/users/${name}`);
+      break; // ошибок не было, выходим из цикла
+    } catch (err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+        // после alert начнётся новая итерация цикла
+        alert('Такого пользователя не существует, пожалуйста, повторите ввод.');
+      } else {
+        // неизвестная ошибка, пробрасываем её
+        throw err;
+      }
+    }
+  }
+
+  alert(`Полное имя: ${user.name}.`);
+  return user;
+}
+
+demoGithubUser();
