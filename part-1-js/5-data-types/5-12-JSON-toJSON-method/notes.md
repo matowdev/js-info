ae78ddc3bf8a5a9568c062bd782da4957fa497e5
Sergey Matylionak <79189126+matowdev@users.noreply.github.com>
2024-08-13 13:56:07 +0300

    [TSK/LES-5.12] JSON-toJSON-method, 1 of 2
    Converting "user" obj to JSON-str and back.
    
    Worth noting:
      - in general JSON format, its application.
    
    part 1 / JS

---
+++ part-1-js/5-data-types/5-12-JSON-toJSON-method/notes.md
@@ -0,0 +1,5 @@
+# On the topic: "Data Types"
+
+### Lesson:
+
+[5.12. JSON format, toJSON method.](https://learn.javascript.ru/json)
---
+++ part-1-js/5-data-types/5-12-JSON-toJSON-method/toJSONFromJSON.js
@@ -0,0 +1,25 @@
+// Преобразуйте объект в JSON, а затем обратно в обычный объект
+// Преобразуйте user в JSON, затем прочитайте этот JSON в другую переменную.
+
+// ! моё решение
+const user = {
+  name: 'Василий Иванович',
+  age: 35,
+};
+console.log(user); // { name: "Василий Иванович", age: 35 }
+
+// преобразование объекта в JSON-строку
+const userToJSON = JSON.stringify(user);
+console.log(userToJSON); // "{"name":"Василий Иванович","age":35}"
+
+// преобразование JSON-строки обратно в объект
+const userFromJSON = JSON.parse(userToJSON);
+console.log(userFromJSON); // { name: "Василий Иванович", age: 35 }
+
+// ?? Решение JS Info
+let user2 = {
+  name: 'Василий Иванович',
+  age: 35,
+};
+
+let user3 = JSON.parse(JSON.stringify(user2));
